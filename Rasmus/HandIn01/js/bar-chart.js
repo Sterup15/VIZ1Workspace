3de0c567d17cc6319787c4c44202f8558b774db3
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2026-09-07 12:48:17 +0200

    Hand-in 01 inital commit Rasmus

---
+++ Rasmus/HandIn01/js/bar-chart.js
@@ -0,0 +1,14 @@
+function createBarChart(data) {
+    const width = 500;
+    const height = 120;
+    const barHeight = 20;
+    const barSpacing = 5;
+    const labelWidth = 100;
+    const labelOffset = 15;
+    const lineX = 200;
+
+    const topGenresSection = d3.select('#top-genres');
+    topGenresSection.append('h3').text('Top US genres, by US on-demand audio streams');
+
+// Continue here
+}
